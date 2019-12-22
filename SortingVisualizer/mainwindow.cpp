b91254ffb608822d30cc9b57f743f9bd44cc9ad8
nishidate-yuki <asta18425@gmail.com>
2019-12-22 18:36:18 +0900

    Add: Ascending, Descending

--- SortingVisualizer/mainwindow.cpp
+++ SortingVisualizer/mainwindow.cpp
@@ -40,11 +40,11 @@ void MainWindow::simpleSort()
     qDebug() << "simpleSort()";
     for(int i=0; i<arraySize; i++){
         for(int j=i+1;j<arraySize;j++){
-            sleep();
-            draw(i, j);
             if(array[j] < array[i]){
                 array.swapItemsAt(i, j);
             }
+            sleep();
+            draw(i, j);
         }
     }
     draw();
@@ -55,10 +55,10 @@ void MainWindow::insertionSort()
     qDebug() << "insertionSort()";
     for(int i=1; i<arraySize; i++){
         for(int j=i-1; j>=0; j--){
-            sleep();
-            draw(i, j);
             if(array[j] > array[j+1]){
                 array.swapItemsAt(j, j+1);
+                sleep();
+                draw(i, j);
             }else break;
         }
     }
@@ -74,10 +74,10 @@ void MainWindow::shellSort()
         h /= 3;
         for(int i=h; i<arraySize; i++){
             for(int j=i-h; j>=0; j-=h){
-                sleep();
-                draw(j, j+h);
                 if(array[j] > array[j+h]){
                     array.swapItemsAt(j, j+h);
+                    sleep();
+                    draw(j, j+h);
                 }else break;
             }
         }
@@ -90,11 +90,11 @@ void MainWindow::bubbleSort()
     qDebug() << "bubbleSort()";
     for(int i=0; i<arraySize-1; i++){
         for(int j=arraySize-1; j>i; j--){
-            sleep();
-            draw(i, j);
             if(array[j-1] > array[j]){
                 array.swapItemsAt(j-1, j);
             }
+            sleep();
+            draw(i, j);
         }
     }
     draw();
@@ -106,14 +106,13 @@ void MainWindow::selectionSort()
     for(int i=0; i<arraySize; i++){
         int minPos = i;
         for(int j=i+1;j<arraySize;j++){
-            sleep();
-            draw(i, j);
             if(array[j] < array[minPos]){
                 minPos = j;
             }
+            sleep();
+            draw(i, j);
         }
         array.swapItemsAt(i, minPos);
-//        draw(i, minPos);
     }
     draw();
 }
@@ -132,12 +131,26 @@ void MainWindow::generate()
     isSorted = false;
 
     arraySize = ui->comboBox->currentText().toInt();
-    for(int i=0; i<arraySize; i++){
-        array.append(qrand()%256);
-        QBrush brush = QBrush(QColor(255,255-array[i],0));
-        scene->addRect(barWidth*i, 0, barWidth, -array[i], pen, brush);
-        view->fitInView(scene->sceneRect());
+    if(ui->comboBox_3->currentText() == "Random"){
+        for(int i=0; i<arraySize; i++){
+            array.append(qrand()%255+1);
+            QBrush brush = QBrush(QColor(255,255-array[i],0));
+            scene->addRect(barWidth*i, 0, barWidth, -array[i], pen, brush);
+        }
+    }else if(ui->comboBox_3->currentText() == "Ascending"){
+        for (int i=0;i<arraySize;i++) {
+            array.append(i*255/arraySize);
+            QBrush brush = QBrush(QColor(255,255-(array[i]),0));
+            scene->addRect(barWidth*i, 0, barWidth, -array[i], pen, brush);
+        }
+    }else if(ui->comboBox_3->currentText() == "Descending"){
+        for (int i=0;i<arraySize;i++) {
+            array.append(255 - i*255/arraySize);
+            QBrush brush = QBrush(QColor(255,255-array[i],0));
+            scene->addRect(barWidth*i, 0, barWidth, -array[i], pen, brush);
+        }
     }
+    view->fitInView(scene->sceneRect());
 }
 
 void MainWindow::resetScene()
